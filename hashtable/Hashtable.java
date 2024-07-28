95389c926f03b1790caa061c2e6cf2f96ed13c95
Andy Birt <abirt1986@gmail.com>
2024-07-28 15:09:43 -0400

    currently dealing with collision issues but have more hashtable functionality

--- hashtable/Hashtable.java
+++ hashtable/Hashtable.java
@@ -1,58 +1,164 @@
-import java.util.ArrayList;
-
 class HashTable<K,V> {
 
-    ArrayList<Node<V>> al;
+    private Node<K,V>[] nodes;
 
+    private float loadFactor;
+
+    private int DEFAULT_SIZE = 10;
+
+    private float DEFAULT_LOAD_FACTOR = 0.75f;
+
+    @SuppressWarnings("unchecked")
     public HashTable() {
-        al = new ArrayList<Node<V>>();
+        nodes = (Node<K,V>[]) new Node[DEFAULT_SIZE];
+        loadFactor = DEFAULT_LOAD_FACTOR;
     }
 
-    public Integer hashCode(K key) {
-        return key.hashCode()%10;
+    @SuppressWarnings("unchecked")
+    public HashTable(int initCapacity) {
+        nodes = (Node<K,V>[]) new Node[initCapacity];
+        loadFactor = DEFAULT_LOAD_FACTOR;
     }
 
+    public int size() {
+        // Iterate the nodes and find everything that isn't null
+        int count = 0;
+        for (int i = 0; i < nodes.length; i++) {
+            if (nodes[i] != null) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public Node<K,V> get(K key) {
+        // Get the index based on the key
+        // If the node is null or the input key doesn't match the found value then return null
+        Node<K,V> node = nodes[this.getIndex(key)];
+        if (node != null && key.equals(node.key)) {
+            return node;
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void put(K key, Node<K,V> node) {
+        // If node array is not big enough to fit new value then increase the size
+        // Place the node in the array with the key
+        if (isNodesFull()) {
+            // System.out.println("nodes full when adding: "+key);
+            Node<K,V>[] oldNodes = nodes;
+            this.nodes = (Node<K,V>[]) new Node[nodes.length * 2];
+            for (Node<K,V> repNode: oldNodes) {
+                // System.out.println(repNode);
+                if (repNode != null) {
+                    int newIndex = getIndex(repNode.key);
+                    System.out.println(newIndex);
+                    nodes[newIndex] = repNode;
+                }
+            }
+            nodes[getIndex(key)] = node;
+        } else {
+            nodes[getIndex(key)] = node;
+        }
+    }
+
+    public void remove(K key) {
+        // Remove the value based on key
+        nodes[getIndex(key)] = null;
+    }
+
+    public void removeAll() {
+        // Set all nodes to null, basically an empty HashTable
+        for (int i = 0; i < nodes.length; i++) {
+            nodes[i] = null;
+        }
+    }
+
+    private int getIndex(K key) {
+        // Generate hashcode to find our node array index
+        System.out.println(key);
+        return key.hashCode() % nodes.length;
+    }
+
+    private double getLoadFactor() {
+        return (double)size() / nodes.length;
+    }
+
+    private boolean isNodesFull() {
+        return getLoadFactor() > loadFactor;
+    }
     
     public static void main(String[] args) {
-        // Create 2 hashtables
-        HashTable<Integer, String> ht = new HashTable<Integer, String>();
-        HashTable<String, Integer> ht2 = new HashTable<String, Integer>();
+        HashTable<String, String> ht = new HashTable<String, String>();
+        //------------- Test adding one value then remove then add again with different value
+        // ht.put("123", new Node<String, String>("123", "testing"));
+        // System.out.println(ht.get("123").value);
+        // System.out.println(ht.size());
 
-        // Initialize the capacity
-        for (int i = 0; i < 10; i++) {
-            ht.al.add(null);
-            ht2.al.add(null);
+        // ht.remove("123");
+        // System.out.println(ht.get("123"));
+        // System.out.println(ht.size());
+
+        // ht.put("123", new Node<String, String>("123","testing again"));
+        // System.out.println(ht.get("123").value);
+        // System.out.println(ht.size());
+
+        // //------------- Test adding 10 values
+
+        // for (int i = 0; i < 10; i++) {
+        //     String key = "key test "+i;
+        //     ht.put(key, new Node<String, String>(key, "testing "+i));
+        // }
+
+        // for (int i = 0; i < 10; i++) {
+        //     // expect values
+        //     String key = "key test "+i;
+        //     System.out.println(ht.get(key).value);
+
+        //     // expect nulls
+        //     System.out.println(ht.get("key "+i));
+        // }
+
+        // // Remove all values;
+        // ht.removeAll();
+
+        // System.out.println(ht.size());
+
+        //------------- Test adding more than 10 values
+
+        for (int i = 0; i < 20; i++) {
+            String key = "key test "+i;
+            ht.put(key, new Node<String, String>(key, "testing "+i));
         }
-        
-        // Hashtable 1 key/value
-        Integer key = 1234;
-        String val = "hey";
 
-        // Hashtable 2 key/value
-        String key2 = "1234";
-        Integer val2 = 999;
+        // for (int i = 0; i < 20; i++) {
+        //     if (ht.nodes[i] == null) {
+        //         System.out.println("null");
+        //     } else {
+        //         System.out.println(ht.nodes[i].value);
+        //     }
+        // }
 
-        // Generate hashcode for each key in each table
-        Integer hashCode = ht.hashCode(key);
-        Integer hashCode2 = ht2.hashCode(key2);
+        // for (int i = 0; i < 20; i++) {
+        //     // expect values
+        //     String key = "key test "+i;
+        //     System.out.println(ht.get(key).value);
 
-        // Set the value for the key in hashtable 1
-        ht.al.set(hashCode, new Node<String>(val));
-        System.out.println(ht.al.get(hashCode).value);
-        System.out.println(hashCode);
+        //     // expect nulls
+        //     System.out.println(ht.get("key "+i));
+        // }
 
-        // Set the value for the key in hashtable 2
-        ht2.al.set(hashCode2, new Node<Integer>(val2));
-        System.out.println(ht2.al.get(hashCode2).value);
-        System.out.println(hashCode2);
     }
 
-    public static class Node<E> {
+    public static class Node<K, V> {
 
-        public E value;
+        public K key;
+        public V value;
 
-        public Node(E v) {
-            this.value = v;
+        public Node(K key, V val) {
+            this.key = key;
+            this.value = val;
         }
     }
 }
